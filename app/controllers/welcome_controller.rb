560fe0272294cf410acd59f234d17fe597710418
lucas.moura.san <lucas.moura.san@gmail.com>
2018-05-25 12:29:23 -0400

    First Commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  
+  def index
+    
+  end
+  
+  def testemarlon
+    
+  end
+  
+end
