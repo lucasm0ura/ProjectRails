25cbfa11942d574f5962765ec1ac98257b9527be
lucas.b1 <lucas.b1@live.com>
2018-07-10 14:52:46 -0400

    Fix Test implement calculate price x amount and show total estimate

--- app/assets/javascripts/jquery.calls.js
+++ app/assets/javascripts/jquery.calls.js
@@ -65,8 +65,7 @@ jQuery( document ).ready( function() {
   $("#client_phone_number").mask('(00) 0000-0000');
   $("#client_celphone").mask('(00) 0 0000-0000');
   $("#client_zipcode").mask('00000-000');
-  $('#service_price').mask('000.000.000.000.000,00', {reverse: true});
-  
+   active_mask(null);
   
   $("#button_create_client").click(function(){
     var phone = $("#client_phone_number").val();
@@ -78,8 +77,58 @@ jQuery( document ).ready( function() {
     }    
    });
   
+  
+ $("#insert_service").bind( "click", function() {
+  var service_selected = $('#services_list').find(":selected").text();
+  var service_id = $('#services_list').find(":selected").val();
+  var mount_html =  '<tr>';
+      mount_html += '<td class="text-center">'+service_id+'</td>';
+      mount_html += '<td class="text-center" style="width: 725px;">'+service_selected+'</td>';
+      mount_html += '<td class="text-center"><input type="text" class="form-control" id="service_price'+service_id+'" onkeyup="calculate_amount_x_service('+service_id+')"></td>';
+      mount_html += '<td class="text-center"><input type="text" class="form-control" size="2" maxlength="2" id="amount_estimate" onkeyup="calculate_amount_x_service('+service_id+')"></td>';
+      mount_html += '<td id="total_service"></td>';
+      mount_html += '</tr>';
+   
+   
+   $('#tableServices > tbody').append(mount_html);
+   
+   active_mask(service_id);
+ });  
 
 });
+
+
+function active_mask (service_id) {
+  if(service_id == null){
+    $('#service_price').mask('000.000.000.000.000,00', {reverse: true});
+  }else{
+    $('#service_price'+service_id).mask('000.000.000.000.000,00', {reverse: true});
+  }  
+}
+
+function calculate_amount_x_service(id){
+    var total = 0;
+  var sum_total_estimate_and_price_service = 0;
+    var amount = $('#amount_estimate').val();
+    var price_service = $('#service_price'+id).val().replace(",", ".");
+      if(amount === "" || amount == null){
+        total = price_service;
+      }else{
+        total = price_service * amount;
+      }
+  
+    var total_estimate = document.getElementById("total_estimate").innerText;
+    if(total_estimate === "" || total_estimate == null){
+      sum_total_estimate_and_price_service = parseFloat(total);
+    }else{
+      sum_total_estimate_and_price_service = parseFloat(total) + parseFloat(total_estimate);
+    }
+    
+    document.getElementById("total_service").innerHTML = total;
+    document.getElementById("total_estimate").innerHTML = sum_total_estimate_and_price_service;  
+}
+
+
 
   function processing_xls_job(job_id, origin_graph){
     job_id = job_id
@@ -91,7 +140,7 @@ jQuery( document ).ready( function() {
     console.log('sending ' + job_id + ' to /percentage_done')
     $.get('/api/v1/tables/verify_export/',{ job_id: job_id } ,function(data){
       console.log(data)
-      if (data.status != false){
+      if (data.status !== false){
           btn_send.style.display = "none";
           loading_image.style.display = "block";
 
